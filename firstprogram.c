85c0e6a2cc5a99a76977ec107675e0ab2b4ec1a7
Ankit Gupta <29058050+luckyhero613@users.noreply.github.com>
2019-10-14 20:12:49 +0530

    Create firstprogram.c
    Create a first program

---
+++ firstprogram.c
@@ -0,0 +1,8 @@
+#include<stdio.h>                 //Library file access
+ 
+int main(){                       //Main program started
+
+  printf("Hello world\n");        //Display Hello world on screen
+  
+  return 0;                 
+} 
